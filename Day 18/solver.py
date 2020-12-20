4a19fb643bb1178a905dbef2119604d64d6cad64
Bas Janssen <smh.janssen@gmail.com>
2020-12-20 22:59:06 +0100

    Code for Day 18 part 2

--- Day 18/solver.py
+++ Day 18/solver.py
@@ -45,6 +45,57 @@ class AoC():
             i += 1
         return result
 
+    def solveEquationAdditionPrecedent(self, equation: str) -> int:
+        # Addition now has precedent over multiplication
+        result = 1
+        tokens = equation.split()
+        i = 0
+
+        replaced_equition = []
+        # find all + in the equation, check if they can be solved, and rebuild the equation
+        # once done, parse the entire equation
+
+        additions = [x[0] for x in enumerate(tokens) if x[1] == "+"]
+
+        while i < len(tokens):
+            if i + 1 in additions:
+                if "(" not in tokens[i] and ")" not in tokens[i] and "(" not in tokens[i + 2] and ")" not in tokens[i + 2]:
+                    temp = int(tokens[i]) + int(tokens[i + 2])
+                    replaced_equition.append(str(temp))
+                    i += 3
+                else:
+                    replaced_equition.append(tokens[i])
+                    i += 1
+            else:
+                replaced_equition.append(tokens[i])
+                i += 1
+
+        solved_sub_equations = []
+        i = 0
+        while i < len(replaced_equition):
+            if "(" in replaced_equition[i]:
+                closing = i
+                _equation = "".join(replaced_equition[i])
+                while _equation.count("(") != _equation.count(")"):
+                    _equation = " ".join(replaced_equition[i:closing + 1])
+                    closing += 1
+
+                _equation = _equation[1:-1]
+                solved_sub_equations.append(str(self.solveEquationAdditionPrecedent(_equation)))
+                i = closing
+            else:
+                solved_sub_equations.append(replaced_equition[i])
+                i += 1
+
+        if "+" not in solved_sub_equations:
+            for token in solved_sub_equations:
+                if token != "*":
+                    result *= int(token)
+        else:
+            result = self.solveEquationAdditionPrecedent(" ".join(solved_sub_equations))
+
+        return result
+
     def Task1(self) -> int:
         result = 0
         for line in self.data:
@@ -53,7 +104,11 @@ class AoC():
         return result
 
     def Task2(self) -> int:
-        pass
+        result = 0
+        for line in self.data:
+            result += self.solveEquationAdditionPrecedent(line)
+
+        return result
 
     def Run(self, filename):
         self.ParseInput(filename)
--- Day 18/tests.py
+++ Day 18/tests.py
@@ -34,6 +34,36 @@ class SolverUnitTests(unittest.TestCase):
         result = solver.solveEquation("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
         self.assertEqual(13632, result)
 
+    def test_solveEquationAdditionPrecedent(self):
+        solver = AoC()
+        result = solver.solveEquationAdditionPrecedent("1 + 2 * 3 + 4 * 5 + 6")
+        self.assertEqual(231, result)
+
+    def test_solvesolveEquationAdditionPrecedent_with_parentheses(self):
+        solver = AoC()
+        result = solver.solveEquationAdditionPrecedent("1 + (2 * 3) + (4 * (5 + 6))")
+        self.assertEqual(51, result)
+
+    def test_solvesolveEquationAdditionPrecedent_with_parentheses_2(self):
+        solver = AoC()
+        result = solver.solveEquationAdditionPrecedent("2 * 3 + (4 * 5)")
+        self.assertEqual(46, result)
+
+    def test_solvesolveEquationAdditionPrecedent_with_parentheses_3(self):
+        solver = AoC()
+        result = solver.solveEquationAdditionPrecedent("5 + (8 * 3 + 9 + 3 * 4 * 3)")
+        self.assertEqual(1445, result)
+
+    def test_solvesolveEquationAdditionPrecedent_with_parentheses_4(self):
+        solver = AoC()
+        result = solver.solveEquationAdditionPrecedent("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
+        self.assertEqual(669060, result)
+        
+    def test_solvesolveEquationAdditionPrecedent_with_high_complexity(self):
+        solver = AoC()
+        result = solver.solveEquationAdditionPrecedent("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+        self.assertEqual(23340, result)
+
     def test_Task1(self):
         solver = AoC()
         solver.data = ["1 + 2 * 3 + 4 * 5 + 6", "1 + (2 * 3) + (4 * (5 + 6))", "2 * 3 + (4 * 5)"]
@@ -42,9 +72,9 @@ class SolverUnitTests(unittest.TestCase):
 
     def test_Task2(self):
         solver = AoC()
-        # solver.data = [0, 3, 6]
-        # result = solver.Task2()
-        # self.assertEqual(175594, result)
+        solver.data = ["1 + 2 * 3 + 4 * 5 + 6", "1 + (2 * 3) + (4 * (5 + 6))", "2 * 3 + (4 * 5)"]
+        result = solver.Task2()
+        self.assertEqual(328, result)
 
 if __name__ == '__main__':
     unittest.main()
